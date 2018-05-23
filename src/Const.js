43a8ed62f77d3266dc129d7cb3cc96b2e8ff8637
Bynn <865068275@qq.com>
2018-05-23 21:35:55 +0800

    add
    add function getRandomColor

--- src/Const.js
+++ src/Const.js
@@ -26,3 +26,19 @@ export default class Const {
 
   static SCREEN_HEIGHT = parseInt(height); //屏幕高度
 }
+
+/**
+ * 獲取随机颜色，一般调试使用 
+ */
+export const getRandomColor = () => {
+  //颜色字符串  
+  var colorStr = "#";
+  //字符串的每一字符的范围  
+  var randomArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+  //产生一个六位的字符串  
+  for (var i = 0; i < 6; i++) {
+    //15是范围上限，0是范围下限，两个函数保证产生出来的随机数是整数  
+    colorStr += randomArr[Math.ceil(Math.random() * (15 - 0) + 0)];
+  }
+  return colorStr;
+}
