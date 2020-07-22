d870cbbc1763c9e461ba813b360724f6781d5645
Barry <865068275@qq.com>
2020-07-22 18:06:33 +0800

    提交

--- templateJS/template/babel.config.js
+++ templateJS/template/babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.jsx', 'js'],
+        alias: {
+          '@': './src',
+          '@utilities': './src/utilities',
+          '@utilities/': './src/utilities',
+        },
+      },
+    ],
+  ],
 };
